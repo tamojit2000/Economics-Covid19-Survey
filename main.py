5d0b1c56b54dd877d3dea9af6bc0d6b6ba98a226
tamojit2000 <tamojitdas2000@gmail.com>
2020-05-15 22:12:41 +0530

    Updated

--- main.py
+++ main.py
@@ -2,13 +2,38 @@ from flask import Flask,render_template,request,redirect
 import pandas as pd
 
 app=Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/submit',methods=['POST','GET'])
-def submit():
+@app.route('/agree',methods=['POST','GET'])
+def agree():
+    if request.method=='POST':
+        return render_template('agree.html')
+    else:
+        return redirect('/')
+
+@app.route('/disagree',methods=['POST','GET'])
+def disagree():
+    if request.method=='POST':
+        return render_template('disagree.html')
+    else:
+        return redirect('/')
+
+@app.route('/submit_agree',methods=['POST','GET'])
+def submit_agree():
     if request.method=='POST':
         d={}
         d['Email']=request.form['Email']
@@ -40,11 +65,16 @@ def submit():
             f.write(str(i)+'-'+str(d[i])+'\n')
         f.close()
 
-        return render_template('submit.html')
+        return render_template('submit_agree.html')
 
     else:
         return redirect('/')
 
+
+
+@app.route('/submit_disagree',methods=['POST','GET'])
+def submit_disagree():
+    return render_template('submit_disagree.html')
 
 
 if __name__=='__main__':
