f694d3a52e7bc210391858c6665d5b8f36dc8555
tamojit2000 <tamojitdas2000@gmail.com>
2020-05-17 01:40:49 +0530

    Update

--- main.py
+++ main.py
@@ -18,7 +18,13 @@ def add_header(response):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    f=open('counter.txt','r')
+    count=int(f.read())
+    f.close()
+    f=open('counter.txt','w')
+    f.write(str(count+1))
+    f.close()
+    return render_template('index.html',counter=count)
 
 @app.route('/agree',methods=['POST','GET'])
 def agree():
@@ -179,10 +185,6 @@ def submit_agree():
         d['Primary_key']=str(datetime.datetime.now())
 
 
-##        f=open('file.txt','w')
-##        for i in d:
-##            f.write(str(i)+'-'+str(d[i])+'\n')
-##        f.close()
 
         scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
         creds = ServiceAccountCredentials.from_json_keyfile_name("creds_1.json", scope)
@@ -190,7 +192,7 @@ def submit_agree():
         sheet = client.open("General_Report").sheet1
         data = sheet.get_all_records()
         no_rows=len(data)
-        
+
         insertRow = [d['Primary_key'],
                      d['Email'],
                      d['Resident_1'],
@@ -215,9 +217,135 @@ def submit_agree():
                      d['Covid19_infected'],
                      d['Covid19_lost_job'],
                      d['Domestic_help_available']]
-                     
+
 
         sheet.insert_row(insertRow,no_rows+2)
+
+
+
+
+
+        sheet = client.open("Detail_Report").sheet1
+        data = sheet.get_all_records()
+        no_rows=len(data)
+
+        insertRow = [d['Primary_key'],
+                     d['Relationship_1'],
+                     d['Age_1'],
+                     d['Gender_1'],
+                     d['Marital_Status_1'],
+                     d['Employment_Status_1'],
+                     d['Occupation_1'],
+                     d['Industry_1'],
+                     d['Education_1'],
+                     d['Disability_1'],
+                     d['Respondent_1'],
+                     d['Work_from_home_1'],
+                     d['Member_in_household_1'],
+                     d['Temporary_in_household_1'],
+
+                     d['Relationship_2'],
+                     d['Age_2'],
+                     d['Gender_2'],
+                     d['Marital_Status_2'],
+                     d['Employment_Status_2'],
+                     d['Occupation_2'],
+                     d['Industry_2'],
+                     d['Education_2'],
+                     d['Disability_2'],
+                     d['Respondent_2'],
+                     d['Work_from_home_2'],
+                     d['Member_in_household_2'],
+                     d['Temporary_in_household_2'],
+
+                     d['Self_Care_bool'],
+                     d['Self_Care_total_hr'],
+                     d['Self_Care_avg_hr'],
+
+                     d['Travel_bool'],
+                     d['Travel_total_hr'],
+                     d['Travel_avg_hr'],
+
+                     d['Work_bool'],
+                     d['Work_total_time'],
+                     d['Work_avg_time'],
+
+                     d['Eating_bool'],
+                     d['Eating_total_time'],
+                     d['Eating_avg_time'],
+
+                     d['Sleeping_bool'],
+                     d['Sleeping_total_time'],
+                     d['Sleeping_avg_time'],
+
+                     d['Studying_bool'],
+                     d['Studying_total_time'],
+                     d['Studying_avg_time'],
+
+                     d['Leisure_bool'],
+                     d['Leisure_total_time'],
+                     d['Leisure_avg_time'],
+
+                     d['Job_searching_bool'],
+                     d['Job_searching_total_time'],
+                     d['Job_searching_avg_time'],
+
+                     d['Household_work_bool'],
+                     d['Household_work_total_time'],
+                     d['Household_work_avg_time'],
+
+                     d['Cooking_bool'],
+                     d['Cooking_total_time'],
+                     d['Cooking_avg_time'],
+
+                     d['Utensils_clean_bool'],
+                     d['Utensils_clean_total_time'],
+                     d['Utensils_clean_avg_time'],
+
+                     d['Washing_clothes_bool'],
+                     d['Washing_clothes_total_time'],
+                     d['Washing_clothes_avg_time'],
+
+                     d['Ironing_clothes_bool'],
+                     d['Ironing_clothes_total_time'],
+                     d['Ironing_clothes_avg_time'],
+
+                     d['Household_clean_bool'],
+                     d['Household_clean_total_time'],
+                     d['Household_clean_avg_time'],
+
+                     d['Teaching_children_bool'],
+                     d['Teaching_children_total_time'],
+                     d['Teaching_children_avg_time'],
+
+                     d['Elderly_person_care_bool'],
+                     d['Elderly_person_care_total_time'],
+                     d['Elderly_person_care_avg_time'],
+
+                     d['Shoping_bool'],
+                     d['Shoping_total_time'],
+                     d['Shoping_avg_time'],
+
+                     d['Outside_time_bool'],
+                     d['Outside_time_total_time'],
+                     d['Outside_time_avg_time'],
+
+                     d['Preparing_food_activity'],
+                     d['Cleaning_food_activity'],
+                     d['Cleaning_clothes_activity'],
+                     d['Folding_clothes_activity'],
+                     d['Ironing_clothes_activity'],
+                     d['Brooming_activity'],
+                     d['Mopping_activity'],
+                     d['Children_activity'] ]
+
+
+
+
+
+
+        sheet.insert_row(insertRow,no_rows+2)
+
 
         return render_template('submit_agree.html')
 
@@ -230,16 +358,12 @@ def submit_agree():
 def submit_disagree():
     if request.method=='POST':
         d={}
-        
+
         d['Resident_1']=request.form['Resident1']
         d['Resident_2']=request.form['Resident2']
         d['Income']=request.form['Income']
         d['Reason']=request.form['Reason']
 
-        f=open('file.txt','w')
-        for i in d:
-            f.write(str(i)+'-'+str(d[i])+'\n')
-        f.close()
         return render_template('submit_disagree.html')
 
     else:
